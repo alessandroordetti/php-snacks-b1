6bee6f6b7c07f69c7af8df0bf7eaaea7080d2e68
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-20 12:15:46 +0200

    Exercise 2 completed

--- index1.php
+++ index1.php
@@ -14,9 +14,28 @@
     $mail = $_GET['mail'];
     $age = $_GET['age'];
 
-    if (strlen($name) > 3 && strpos($mail, '@') && is_numeric($age)) {
+    if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
         echo 'OK';
     } else {
         echo 'Accesso negato';
     }
+
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            
+        ?>
+    </p>
+</body>
+</html>
